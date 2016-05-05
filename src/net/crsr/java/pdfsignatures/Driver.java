171aac6b58656ba08b60a5f1cf58cc31d41b229e
Tommy M. McGuire <mcguire@crsr.net>
2016-05-05 18:45:45 -0500

    Add simplified signing test
    * Added sign-created command to Driver and Listing1214.java. This is not
    the Listing1214 from the book, but a bastardized version combining it
    with the white-paper documentation (since things have changed since the
    book).
    
    * Added the Bouncy Castle crypto jars used by iText.
    
    * Added a genkeystore script to create a keystore usable by Listing1214.
    
    White-paper:
    
    http://developers.itextpdf.com/examples/security/digital-signatures-white-paper

--- src/net/crsr/java/pdfsignatures/Driver.java
+++ src/net/crsr/java/pdfsignatures/Driver.java
@@ -1,22 +1,37 @@
 package net.crsr.java.pdfsignatures;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 import com.itextpdf.text.DocumentException;
 
 import net.crsr.java.pdfsignatures.examples.Listing1213;
+import net.crsr.java.pdfsignatures.examples.Listing1214;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		try {
 			if (args.length == 0) {
-				System.err.println("Usage: Driver create filename");
+				System.err
+						.println("Usage: Driver (create <filename> | sign-created <in> <out> <keystore> <store-pass> <key-pass>)");
 				System.exit(-1);
 			} else if (args[0].equals("create")) {
-				new Listing1213().createPdf(args[1]);
+				String filename = (args.length > 1) ? args[1] : "test.pdf";
+				new Listing1213().createPdf(filename);
+			} else if (args[0].equals("sign-created")) {
+				String inputFilename = (args.length > 1) ? args[1] : "test.pdf";
+				String outputFilename = (args.length > 2) ? args[2] : "test-signed.pdf";
+				String keystoreName = (args.length > 3) ? args[3] : "keystore";
+				String keystorePass = (args.length > 4) ? args[4] : "keystorePass";
+				String keyPass = (args.length > 5) ? args[5] : "keyPass";
+				Listing1214.setupBouncyCastle();
+				new Listing1214(keystoreName, keystorePass, keyPass).signPdf(inputFilename, outputFilename);
+				;
+			} else {
+				System.err.println("Unknown command: " + args[0]);
 			}
-		} catch (IOException | DocumentException e) {
+		} catch (IOException | DocumentException | GeneralSecurityException e) {
 			e.printStackTrace();
 		}
 	}
---
+++ src/net/crsr/java/pdfsignatures/examples/Listing1214.java
@@ -0,0 +1,68 @@
+package net.crsr.java.pdfsignatures.examples;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.cert.Certificate;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfSignatureAppearance;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.security.BouncyCastleDigest;
+import com.itextpdf.text.pdf.security.DigestAlgorithms;
+import com.itextpdf.text.pdf.security.ExternalDigest;
+import com.itextpdf.text.pdf.security.ExternalSignature;
+import com.itextpdf.text.pdf.security.MakeSignature;
+import com.itextpdf.text.pdf.security.MakeSignature.CryptoStandard;
+import com.itextpdf.text.pdf.security.PrivateKeySignature;
+
+public class Listing1214 {
+	String											keystorePath;
+	String											keystorePassword;
+	String											keyPassword;
+
+	private static final String	digestAlgorithm	= DigestAlgorithms.SHA256;
+
+	public Listing1214(String keystorePath, String keystorePassword, String keyPassword) {
+		this.keystorePath = keystorePath;
+		this.keystorePassword = keystorePassword;
+		this.keyPassword = keyPassword;
+	}
+
+	public static void setupBouncyCastle() {
+		Security.addProvider(new BouncyCastleProvider());
+	}
+
+	public void signPdf(String inputFilename, String outputFilename)
+			throws FileNotFoundException, IOException, DocumentException, GeneralSecurityException {
+		KeyStore ks = KeyStore.getInstance(/* "pkcs12" */ "JKS");
+		ks.load(new FileInputStream(keystorePath), keystorePassword.toCharArray());
+		// keystorePassword.toCharArray());
+		String alias = (String) ks.aliases().nextElement();
+		PrivateKey pk = (PrivateKey) ks.getKey(alias, keyPassword.toCharArray());
+		Certificate[] chain = ks.getCertificateChain(alias);
+		String provider = BouncyCastleProvider.PROVIDER_NAME;
+		// Creating the reader and the stamper
+		PdfReader reader = new PdfReader(inputFilename);
+		FileOutputStream os = new FileOutputStream(outputFilename);
+		PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
+		// Creating the appearance
+		PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
+		appearance.setReason("It's personal");
+		appearance.setLocation("Foobar");
+		appearance.setVisibleSignature(new Rectangle(36, 748, 144, 780), 1, "sig");
+		// Creating the signature
+		ExternalDigest digest = new BouncyCastleDigest();
+		ExternalSignature signature = new PrivateKeySignature(pk, digestAlgorithm, provider);
+		MakeSignature.signDetached(appearance, digest, signature, chain, null, null, null, 0, CryptoStandard.CMS);
+	}
+}
